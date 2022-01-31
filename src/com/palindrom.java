a6cf45f905b0a11beb0f7a7d4ce87b9c130c559c
Fjolnirr <yusufcan2617@gmail.com>
2022-01-31 11:24:39 +0300

    palindrom class added

---
+++ src/com/palindrom.java
@@ -0,0 +1,27 @@
+package com;
+
+public class palindrom {
+    public static void main(String[] args) {
+
+        int num = 123321, reversedNum = 0, remainder;
+
+        // store the number to originalNum
+        int originalNum = num;
+
+        // get the reverse of originalNum
+        // store it in variable
+        while (num != 0) {
+            remainder = num % 10;
+            reversedNum = reversedNum * 10 + remainder;
+            num /= 10;
+        }
+
+        // check if reversedNum and originalNum are equal
+        if (originalNum == reversedNum) {
+            System.out.println(originalNum + " is Palindrome.");
+        }
+        else {
+            System.out.println(originalNum + " is not Palindrome.");
+        }
+    }
+}
